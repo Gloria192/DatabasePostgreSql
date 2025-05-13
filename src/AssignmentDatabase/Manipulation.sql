63da97bf4176382cd5e28d474fd503fc50d97dd1
Gloria <mutuyegloria@gmail.com>
2025-05-13 08:55:23 -0700

    add the change

--- src/AssignmentDatabase/Manipulation.sql
+++ src/AssignmentDatabase/Manipulation.sql
@@ -1,3 +1,13 @@
-SELECT * FROM Customers;
-SELECT * FROM books;
+
+-- SELECT * FROM books;
+SELECT * FROM ORDERS;
+UPDATE BOOKS SET price = price + 5 WHERE title = 'The Silent Patient';
+
+UPDATE BOOKS SET stock_quantity = 25 WHERE title = 'Atomic Habits';
+DELETE FROM CUSTOMERS WHERE email = 'john.doe@example.com';
+
+DELETE FROM ORDERS WHERE order_id = 3;
+
+SELECT  * FROM books;
 SELECT * FROM orders;
+SELECT * FROM books;
--- src/AssignmentDatabase/insert_table.sql
+++ src/AssignmentDatabase/insert_table.sql
@@ -3,16 +3,16 @@ VALUES
     ('Database Management', 'John Smith', 45.99, 100),
        ('Learn SQL', 'Jane Doe', 39.99, 50),
      ('Python Programming', 'Emily Davis', 55.75, 30),
-  ('Web Development', 'Michael Brown', 29.99, 80);
+  ('Atomic habits', 'Michael Brown', 29.99, 80);
 
-INSERT INTO customers (FIRST_NAME, LAST_NAME, EMAIL, PHONE) VALUES
+INSERT INTO CUSTOMERS(FIRST_NAME, LAST_NAME, EMAIL, PHONE) VALUES
 
 ('Gloria', 'Mutuyimana', 'gloria@example.com', '+250788123456'),
      ('John', 'Doe', 'john@example.com', '+250722345678'),
      ('Jane', 'Smith', 'jane@example.com', '+250733987654'),
      ('Alice', 'Brown', 'alice@example.com', '+250711123456');
 
-INSERT INTO Orders (Customer_id, total_amount) VALUES
+INSERT INTO ORDERS (Customer_id, total_amount) VALUES
   (4, 4555),
   (1, 14500),
   (2, 8000),
